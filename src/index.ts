bd75904709935146db360554ac4a53f869d914fe
Grant Gapinski <gruntygap@gmail.com>
2019-08-20 09:42:52 -0500

    Parsed classData from selected Banner page

--- src/index.ts
+++ src/index.ts
@@ -35,12 +35,52 @@ getFormData()
         const username = '';
         const password = '';
         yeet(username, password, executionParam).then((resp) => {
-            console.log(resp.text);
+            // console.log(resp.text);
             html = resp.text;
             parsable = parser.parse(html);
-            // const names = parsable.querySelectorAll('.captiontext').map((yote) => yote.text).filter((yote) => yote !== 'Scheduled Meeting Times');
-            // console.log({names});
-            debugger;
+            const names = parsable.querySelectorAll('.captiontext')
+                .map((yote: any) => yote.text).filter((yote: any) => yote !== 'Scheduled Meeting Times');
+            const metaData = parsable.querySelector('.pagebodydiv')
+                .querySelectorAll('table')
+                .filter((something: any) => something.attributes.SUMMARY === 'This layout table is used to present the schedule course detail');
+            const meetingData = parsable.querySelector('.pagebodydiv')
+                .querySelectorAll('table')
+                .filter((something: any) => something.attributes.SUMMARY === 'This table lists the scheduled meeting times and assigned instructors for this class..');
+            let classData = [];
+            for (let i = 0; i < metaData.length; i++) {
+                const trs = metaData[i].removeWhitespace().querySelectorAll('tr');
+                const event = meetingData[i].removeWhitespace().querySelectorAll('td');
+
+                const term = trs[0].querySelector('td').text;
+                const crn = trs[1].querySelector('td').text;
+                const status = trs[2].querySelector('td').text;
+                const instructor = trs[3].querySelector('TD').text;
+                const credits = trs[5].querySelector('td').text;
+                const level = trs[6].querySelector('td').text;
+                const campus = trs[7].querySelector('td').text;
+
+                const time = event[1].text;
+                const dow = event[2].text;
+                const classroom = event[3].text;
+                const yearConstraints = event[4].text;
+
+                const emptyStructure = {
+                    campus,
+                    classroom,
+                    credits,
+                    crn,
+                    dow,
+                    instructor,
+                    level,
+                    name: names[i],
+                    status,
+                    term,
+                    time,
+                    yearConstraints,
+                };
+                classData.push(emptyStructure);
+            }
+            console.log(classData);
         }).catch((error) => console.error(error));
     })
     .catch((err) => console.error(err));
