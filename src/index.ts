5a91b1e965cf08a8ae3ad2002e378686c83b3177
Grant Gapinski <gruntygap@gmail.com>
2019-08-20 19:47:09 -0500

    Changed parsing to async

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,21 @@ import superagent from 'superagent';
 
 const agent = superagent.agent();
 
-async function getFormData() {
+async function getLoginFormHtml() {
     const request = await agent
         .get('https://auth-prod.bethel.edu/cas/login?service=https%3A%2F%2Fbanner.bethel.edu%2Fssomanager%2Fc%2FSSB');
     return request;
 }
 
-async function yeet(user: string, pass: string, execution: string) {
+async function getExecutionString(html: string) {
+    const parsable = parser.parse(html);
+    const executionElement = parsable.querySelectorAll('input')
+        .filter((thing: any) => thing.attributes.name === 'execution')[0];
+    const executionParam = executionElement.attributes.value;
+    return executionParam;
+}
+
+async function getDetailScheduleHtml(user: string, pass: string, execution: string) {
     const req1 = await agent
         .post('https://auth-prod.bethel.edu/cas/login')
         .type('form')
@@ -25,62 +33,59 @@ async function yeet(user: string, pass: string, execution: string) {
     return req2;
 }
 
-getFormData()
-    .then((res) => {
-        let html = res.text;
-        let parsable = parser.parse(html);
-        const executionElement = parsable.querySelectorAll('input')
-            .filter((thing: any) => thing.attributes.name === 'execution')[0];
-        const executionParam = executionElement.attributes.value;
-        const username = '';
-        const password = '';
-        yeet(username, password, executionParam).then((resp) => {
-            // console.log(resp.text);
-            html = resp.text;
-            parsable = parser.parse(html);
-            const names = parsable.querySelectorAll('.captiontext')
-                .map((yote: any) => yote.text).filter((yote: any) => yote !== 'Scheduled Meeting Times');
-            const metaData = parsable.querySelector('.pagebodydiv')
-                .querySelectorAll('table')
-                .filter((something: any) => something.attributes.SUMMARY === 'This layout table is used to present the schedule course detail');
-            const meetingData = parsable.querySelector('.pagebodydiv')
-                .querySelectorAll('table')
-                .filter((something: any) => something.attributes.SUMMARY === 'This table lists the scheduled meeting times and assigned instructors for this class..');
-            let classData = [];
-            for (let i = 0; i < metaData.length; i++) {
-                const trs = metaData[i].removeWhitespace().querySelectorAll('tr');
-                const event = meetingData[i].removeWhitespace().querySelectorAll('td');
+function parseDetailSchedule(html: string) {
+    const parsable = parser.parse(html);
+    const names = parsable.querySelectorAll('.captiontext').map((yote: any) => yote.text).filter((yote: any) => yote !== 'Scheduled Meeting Times');
+    const metaData = parsable.querySelector('.pagebodydiv')
+        .querySelectorAll('table')
+        .filter((something: any) => something.attributes.SUMMARY === 'This layout table is used to present the schedule course detail');
+    const meetingData = parsable.querySelector('.pagebodydiv')
+        .querySelectorAll('table')
+        .filter((something: any) => something.attributes.SUMMARY === 'This table lists the scheduled meeting times and assigned instructors for this class..');
+    const classData = [];
+    for (let i = 0; i < metaData.length; i++) {
+        const trs = metaData[i].removeWhitespace().querySelectorAll('tr');
+        const event = meetingData[i].removeWhitespace().querySelectorAll('td');
 
-                const term = trs[0].querySelector('td').text;
-                const crn = trs[1].querySelector('td').text;
-                const status = trs[2].querySelector('td').text;
-                const instructor = trs[3].querySelector('TD').text;
-                const credits = trs[5].querySelector('td').text;
-                const level = trs[6].querySelector('td').text;
-                const campus = trs[7].querySelector('td').text;
+        const term = trs[0].querySelector('td').text;
+        const crn = trs[1].querySelector('td').text;
+        const status = trs[2].querySelector('td').text;
+        const instructor = trs[3].querySelector('TD').text;
+        const credits = trs[5].querySelector('td').text;
+        const level = trs[6].querySelector('td').text;
+        const campus = trs[7].querySelector('td').text;
 
-                const time = event[1].text;
-                const dow = event[2].text;
-                const classroom = event[3].text;
-                const yearConstraints = event[4].text;
+        const time = event[1].text;
+        const dow = event[2].text;
+        const classroom = event[3].text;
+        const yearConstraints = event[4].text;
 
-                const emptyStructure = {
-                    campus,
-                    classroom,
-                    credits,
-                    crn,
-                    dow,
-                    instructor,
-                    level,
-                    name: names[i],
-                    status,
-                    term,
-                    time,
-                    yearConstraints,
-                };
-                classData.push(emptyStructure);
-            }
-            console.log(classData);
-        }).catch((error) => console.error(error));
-    })
-    .catch((err) => console.error(err));
+        const emptyStructure = {
+            campus,
+            classroom,
+            credits,
+            crn,
+            dow,
+            instructor,
+            level,
+            name: names[i],
+            status,
+            term,
+            time,
+            yearConstraints,
+        };
+        classData.push(emptyStructure);
+    }
+    return classData;
+}
+export default async function getClassData(username: string, password: string) {
+    try {
+        const loginHtml = await getLoginFormHtml();
+        const executionString = await getExecutionString(loginHtml.text);
+        const detailScheduleHtml = await getDetailScheduleHtml(username, password, executionString);
+        const classes = parseDetailSchedule(detailScheduleHtml.text);
+        return classes;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import getClassData from './index';
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('yeet');
+    res.sendFile('/src/templates/index.html', { root: __dirname + '/..' });
 });
 
-app.post('/submit', (req, res) => {
-    res.send('POST request to the homepage');
-  })
+app.post('/submit', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const classData = await getClassData(username, password);
+        res.send(classData);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 app.get('*', (req, res) => {
     res.status(401).send('HUH? Did you get that URL right?');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
